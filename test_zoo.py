98b653f1f7adb14af0258972f1efe93ecd5d37aa
Kanasorn Sudyodbunphot <kanasorn.sudy@mail.kmutt.ac.th>
2024-11-14 15:18:01 +0700

    fix: zoo, feat: add test case

--- test_zoo.py
+++ test_zoo.py
@@ -4,11 +4,29 @@ from zoo import Zoo
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
+    
+    def test_initail(self):
+        self.assertEqual(self.zoo.get_ticket_price(5), 50) # valid output
+        
+    # Add your additional test cases here.
+    
+    def test_invalid_input(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), None) # valid output
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(0), 50) # # invalid output, expect return 150 but actual return None, Now is valid output
+        self.assertEqual(self.zoo.get_ticket_price(12), 50) # valid output
+        
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100) # valid output
+        self.assertEqual(self.zoo.get_ticket_price(20), 100) # invalid output, expect return 100 but actual return None, Now is valid output
+        
+    def test_worker_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150) # invalid output, expect return 150 but actual return None, Now is valid output
+        self.assertEqual(self.zoo.get_ticket_price(60), 150) # valid output
+        
+    def test_older_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100) # valid output
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: # change 0 < to 0 <=
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: # change < 20 to <= 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: # change 21 < to 21 <=
             return 150
-        elif age >= 60:
+        # on this line have no error but have one false that is joint between worker(21-60)
+        # then i change >= 60 to > 60
+        elif age > 60:
             return 100
